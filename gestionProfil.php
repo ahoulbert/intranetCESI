24e8cbc390f8610f449ebe3f5aefa84c64834ed5
VILLERMIN Maxime <Maxime.VILLERMIN@vinci-energies.net>
2020-02-27 10:12:46 +0100

    gestionUser

--- gestionProfil.php
+++ gestionProfil.php
@@ -1,58 +1,33 @@
 <?php
 include_once("./navbar.php");
 ?>
-
+<html>
 <head>
     <!-- Required meta tags-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Title Page-->
     <title>Mon profil</title>
-
     <!-- Icons font CSS-->
     <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
     <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
     <!-- Font special for pages-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-
-    <!-- Vendor CSS-->
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
-
     <!-- Main CSS-->
     <link href="css/main.css" rel="stylesheet" media="all">
-
-    <!-- Jquery JS-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <!-- Vendor JS-->
-    <script src="vendor/select2/select2.min.js"></script>
-    <script src="vendor/datepicker/moment.min.js"></script>
-    <script src="vendor/datepicker/daterangepicker.js"></script>
-
-    <!-- Main JS-->
-    <script src="js/global.js"></script>
 </head>
-
 <body>
     <div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">
         <div class="wrapper wrapper--w790">
             <div class="card card-5">
                 <div class="card-heading w3-theme-d2">
                     <h4 class="title">Modifier votre profil</h4>
-
                 </div>
                 <div class="card-body">
-
                     <div class="form-row">
-                        <img src="./images/jul.jpg" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px;">
+                        <img src="vues/images/jul.jpg" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px;">
                         <input id="inputChangePicture" type="file" style="display: none" accept="image/png, image/jpeg" />
                         <button id="changePicture" class="btn btn--change-photo  btn--dark">Changer de photo</button>
-                        <script>
-                            $("#changePicture").on("click", function() {
-                                $("#inputChangePicture").trigger("click");
-                            });
-                        </script>
                     </div>
                     <form method="POST">
                         <div class="form-row m-b-55">
@@ -119,15 +94,13 @@ include_once("./navbar.php");
                                         <div class="form-row">
                                             <div class="name">Status</div>
                                             <div class="input-group">
-                                                <div class="rs-select2 js-select-simple select--no-search">
+                                                <div class="select--no-search">
                                                     <div class="p-t-15">
-                                                        <select name="subject">
-                                                            <option disabled="disabled" selected="selected">Choisir une option</option>
+                                                        <select name="subject" class="custom-select sources" placeholder="Choix">
                                                             <option>Alternant</option>
                                                             <option>Etudiant</option>
                                                         </select>
                                                     </div>
-                                                    <div class="select-dropdown"></div>
                                                 </div>
                                             </div>
                                         </div>
@@ -157,6 +130,15 @@ include_once("./navbar.php");
         </div>
     </div>
 </body>
-
 </html>
+<!-- Jquery JS-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<!-- Vendor JS-->
+<script src="vendor/select2/select2.min.js"></script>
+<script src="vendor/datepicker/moment.min.js"></script>
+<script src="vendor/datepicker/daterangepicker.js"></script>
+<script src="vues/js/gestionProfil.js"></script>
+
+<!-- Main JS-->
+<script src="js/global.js"></script>
 <!-- end document-->
