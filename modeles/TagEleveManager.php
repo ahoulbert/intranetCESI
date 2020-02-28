def4828eea809dcc0b4717b08622a7d3f65fb78f
Clément AZIBEIRO <clementazibeiro@sfr.fr>
2020-02-28 09:12:59 +0100

    TagEleve manager

--- modeles/TagEleveManager.php
+++ modeles/TagEleveManager.php
@@ -11,23 +11,67 @@
         }
 
         public function getAllTagEleve() {
+            $result = [];
 
+            $statement = $this->_db->prepare('SELECT * FROM tageleve');
+
+            $statement->execute() or die(print_r($statement->errorInfo()));
+
+            while ($donnees = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+                $result[] = new TagEleve($donnees);
+            }
+
+            return $result;
         }
 
-        public function getTagEleveById($idTagEleve) {
+        public function getTagEleveById($idTag, $mailCESI) {
+            $statement = $this->_db->prepare('SELECT * FROM tageleve WHERE idPost = :idPost and mailCESI = :mailCESI');
+            $statement->bindParam(':mailCESI', $mailCESI, PDO::PARAM_STR);
+            $statement->bindParam(':idGroupe', $idTag, PDO::PARAM_INT);
 
+            $statement->execute() or die(print_r($statement->errorInfo()));
+
+            $donnees = $statement->fetch(PDO::FETCH_ASSOC);
+
+            if($donnees) {
+                return new TagEleve($donnees);
+            } else {
+                return false;
+            }
         }
 
-        public function updateTagEleve(TagEleve $tagEleve) {
+        public function getTagEleveByMail($mailCESI) {
+            $statement = $this->_db->prepare('SELECT * FROM tageleve WHERE mailCESI = :mailCESI');
+            $statement->bindParam(':mailCESI', $mailCESI, PDO::PARAM_STR);
 
+            $statement->execute() or die(print_r($statement->errorInfo()));
+
+            $donnees = $statement->fetch(PDO::FETCH_ASSOC);
+
+            if($donnees) {
+                return new TagEleve($donnees);
+            } else {
+                return false;
+            }
         }
 
         public function createTagEleve(TagEleve $tagEleve) {
+            $statement = $this->_db->prepare("INSERT INTO tageleve
+                                            VALUES (:idTag, :mailCESI)");
 
+            $statement->bindParam(':mailCESI', $tagEleve->getMailCESI(), PDO::PARAM_STR);
+            $statement->bindParam(':idGroupe', $tagEleve->getIdTag(), PDO::PARAM_INT);
+
+            $statement->execute() or die(print_r($statement->errorInfo()));
         }
 
-        public function deleteTagEleve($idTagEleve) {
-            
+        public function deleteTagEleve($idTag, $mailCESI) {
+            $statement = $this->_db->prepare("DELETE FROM tageleve where idTag = :idTag and mailCESI = :mailCESI");
+            $statement->bindParam(':idTag', $idTag, PDO::PARAM_INT);
+            $statement->bindParam(':mailCESI', $mailCESI, PDO::PARAM_STR);
+
+            $statement->execute() or die(print_r($statement->errorInfo()));
         }
 
         public function setDb(PDO $db)
